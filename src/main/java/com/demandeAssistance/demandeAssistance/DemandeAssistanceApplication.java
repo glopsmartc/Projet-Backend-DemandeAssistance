a70383efeb26556eb8207a00a244b4f7d42c7008
rawanghosn <rawanghosn22@gmail.com>
2025-01-20 15:14:19 +0100

    first commit with base structure

---
+++ src/main/java/com/demandeAssistance/demandeAssistance/DemandeAssistanceApplication.java
@@ -0,0 +1,13 @@
+package com.demandeAssistance.demandeAssistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandeAssistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandeAssistanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demandeAssistance/demandeAssistance/DemandeAssistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demandeAssistance.demandeAssistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandeAssistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
